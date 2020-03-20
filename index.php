1d1995ef68305eb55a00a4fdff6624223996ca4e
Thanatkrit <thanatkrit_ket@nationgroup.com>
2020-03-20 11:46:19 +0700

    routes.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-if(empty($_GET['page'])){
-    require_once __DIR__."/web/batch_detail.php";
-}else{
+if(!empty($_GET['page'])){
     require_once __DIR__."/web/{$_GET['page']}.php";
+}elseif (!empty($_GET['data'])){
+    require_once __DIR__."/web/data/{$_GET['data']}.php";
+}else{
+    require_once __DIR__."/web/batch_detail.php";
 }
 
 ?>
